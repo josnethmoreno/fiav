1ae58947a8db12baf7c2718f22b209326ec7c480
Josneth Moreno <josnethmoreno@gmail.com>
2024-06-24 14:51:34 -0400

    fix header button

--- main.js
+++ main.js
@@ -23,55 +23,70 @@ let tl = gsap.timeline({ defaults: { ease: 'power2.out', duration: 2 } })
 tl.to('header', {
 	opacity: 1,
 	duration: 1.2,
-  delay: .6
+	delay: 0.6,
 })
-tl.to('.hero-strong-1', {
-  'clip-path': 'inset(0% 0% 0% 0%)',
-	y: 0,
-	opacity: 1,
-	duration: .8,
-}, "-=.4")
+tl.to(
+	'.hero-strong-1',
+	{
+		'clip-path': 'inset(0% 0% 0% 0%)',
+		y: 0,
+		opacity: 1,
+		duration: 0.8,
+	},
+	'-=.4'
+)
 tl.to('.hero-strong-2', {
-  'clip-path': 'inset(0% 0% 0% 0%)',
+	'clip-path': 'inset(0% 0% 0% 0%)',
 	y: 0,
 	opacity: 1,
-	duration: .8,
+	duration: 0.8,
 })
 tl.to('.hero .button-primary', {
 	opacity: 1,
-	duration: .8,
+	duration: 0.8,
 })
-tl.to('.hero .whatsapp', {
-	opacity: 1,
-	duration: .8,
-}, "-=.6")
+tl.to(
+	'.hero .whatsapp',
+	{
+		opacity: 1,
+		duration: 0.8,
+	},
+	'-=.6'
+)
 
 const text = document.querySelector('[data-text-animation]')
-const splitText = new SplitType(text, {types: 'chars'})
+const splitText = new SplitType(text, { types: 'chars' })
 
 gsap.from(splitText.chars, {
-  scrollTrigger: {
-    trigger: text,
-    start: 'top 80%',
-    end: 'top 20%',
-    scrub: true,
-    markers: false
-  },
-  opacity: 0.2,
-  stagger: 0.1
+	scrollTrigger: {
+		trigger: text,
+		start: 'top 80%',
+		end: 'top 20%',
+		scrub: true,
+		markers: false,
+	},
+	opacity: 0.2,
+	stagger: 0.1,
 })
 
-
 gsap.to('.data-ul', {
-  scrollTrigger: {
-    trigger: '.data-ul',
-    start: 'top 70%',
-    scrub: false,
-    markers: false
-  },
-  opacity: 1,
-  clipPath: 'inset(0% 0% 0% 0%)',
-  stagger: 0.4,
-  ease: 'power2.out',
-  duration: 1
+	scrollTrigger: {
+		trigger: '.data-ul',
+		start: 'top 70%',
+		scrub: false,
+		markers: false,
+	},
+	opacity: 1,
+	clipPath: 'inset(0% 0% 0% 0%)',
+	stagger: 0.4,
+	ease: 'power2.out',
+	duration: 1,
+})
+
+const form = document.getElementById('mc-embedded-subscribe-form')
+
+form.addEventListener('submit', (e) => {
+	setTimeout(() => {
+		e.target.reset()
+	}, 1000)
 })
