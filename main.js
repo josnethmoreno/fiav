f0a9191d5569c465dc174e7e1b8f0aaac867c3ae
Josneth Moreno <josnethmoreno@gmail.com>
2024-07-19 19:28:55 -0400

    fixes

--- main.js
+++ main.js
@@ -16,6 +16,18 @@ gsap.ticker.add((time) => {
 	lenis.raf(time * 600)
 })
 
+document.querySelectorAll('a[href^="#"]').forEach((el) => {
+  el.addEventListener('click', (e) => {
+    e.preventDefault()
+    const id = el.getAttribute('href')?.slice(1)
+    if (!id) return
+    const target = document.getElementById(id)
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' })
+    }
+  })
+})
+
 gsap.ticker.lagSmoothing(0)
 
 let tl = gsap.timeline({ defaults: { ease: 'power2.out', duration: 2 } })
